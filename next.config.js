c3bb22f67c3fa9fcff4c7db6bea468c26d1b937d
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-06-10 12:33:05 +0200

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -20,4 +24,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig)
