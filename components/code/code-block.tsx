45cf2cc4eb227951db7bdef3246d1d80151173ef
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-02-09 12:17:37 +0100

    refactor: change test 2

--- components/code/code-block.tsx
+++ components/code/code-block.tsx
@@ -3,27 +3,25 @@ import React, { ReactNode } from "react";
 import CopyButton from "@/components/code/copy2clipboard";
 import { cn } from "@/lib/utils";
 
+type CodeBlockProps = {
+  children: ReactNode;
+  className?: string;
+  copyString?: string;
+}
+
 export default function CodeBlock({
   children,
   className,
   copyString,
-}: {
-  children: ReactNode;
-  className?: string;
-  copyString?: string;
-}) {
+}: CodeBlockProps) {
   return (
     <div
       className={cn(
         "relative bg-gray-300 grid grid-cols-6 gap-x-2 w-full dark:bg-gray-700 rounded-md p-4 justify-between",
         className,
-      )}
-    >
+      )}>
       <pre
-        className={
-          "text-foreground col-span-5 whitespace-pre-wrap w-full content-center overflow-x-auto "
-        }
-      >
+        className={"text-foreground col-span-5 whitespace-pre-wrap w-full content-center overflow-x-auto"}>
         {children}
       </pre>
       <CopyButton
